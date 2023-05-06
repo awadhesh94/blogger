18dc628f3d5dfdbdb7a3342321808f854a513c5f
Awadhesh Kumar <121618523+awadhesh94@users.noreply.github.com>
2023-05-07 03:04:27 +0530

    page setup (#12)

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
