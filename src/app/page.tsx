4c6fc4d271a39a0d2caaf905d79c5ee713e166e6
awadhesh94 <awadhesh31st@outlook.com>
2023-05-07 03:03:21 +0530

    page setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <h1 className="bg-biege text-coal font-extrabold text-lg">Home</h1>;
+  return (
+    <main className="grid min-h-screen place-items-center bg-coal font-brandLight px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-bold text-indigo-600 text-pink">Home</p>
+        <h1 className="mt-4 text-3xl font-brandBold text-yellow tracking-tight text-gray-900 sm:text-5xl">
+          Comming Soon
+        </h1>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
