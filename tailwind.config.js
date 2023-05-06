ed091bcbeb71546d32d0972e965f0fae567a5ee4
awadhesh94 <awadhesh31st@outlook.com>
2023-05-06 20:32:57 +0530

    update tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,34 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      brandThin: ["font-100", "sans-serif", "serif"],
+      brandExtraLight: ["font-200", "sans-serif", "serif"],
+      brandLight: ["font-300"],
+      brandRegular: ["font-400", "sans-serif", "serif"],
+      brandMedium: ["font-500", "sans-serif", "serif"],
+      brandSemiBold: ["font-600", "sans-serif", "serif"],
+      brandBold: ["font-700", "sans-serif", "serif"],
+      brandExtraBold: ["font-800", "sans-serif", "serif"],
+      brandBlack: ["font-900", "sans-serif", "serif"],
+    },
+    colors: {
+      coal: "#0f0d0e",
+      charcoal: "#231f20",
+      gray: "#262522",
+      yellow: "#fcba28",
+      pink: "#f38ba3",
+      green: "#0ba95b",
+      lightGreen: "#cfe957",
+      purple: "#7b5ea7",
+      biege: "#f9f4da",
+      blue: "#12b5e5",
+      orange: "#fc7428",
+      red: "#ed203d",
+      white: "#ffffff",
     },
   },
   plugins: [],
 }
+
+
